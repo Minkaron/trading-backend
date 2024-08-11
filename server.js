1b48426461d64b2f57a92f65cbb18d108a4e5381
Minkaron <aaron.tucaz@gmail.com>
2024-08-11 15:35:15 -0700

    Fleshing out site

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require("path");
 const app = express();
 const port = 3000
 
+
+app.use("/static/", express.static(path.join(__dirname, "public")));
 app.use(express.static(__dirname));
 
 
